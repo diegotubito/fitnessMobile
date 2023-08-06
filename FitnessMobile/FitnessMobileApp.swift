40add2ad6abd6499eea532703ddc3a4912819ea2
David Diego Gomez <diego@MBP-de-David.fibertel.com.ar>
2023-08-05 23:12:49 -0300

    Coordinator Pattern

--- FitnessMobile/FitnessMobileApp.swift
+++ FitnessMobile/FitnessMobileApp.swift
@@ -10,11 +10,13 @@ import SwiftUI
 @main
 struct FitnessMobileApp: App {
     @StateObject var socketIOManager = SocketIOManager()
+    @StateObject var coordinator = Coordinator()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environmentObject(socketIOManager)
+                .environmentObject(coordinator)
         }
     }
 }
